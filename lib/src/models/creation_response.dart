2dc8651135f4e10c1a75549074ce18a63e587b49
Nick Meinhold <nick.meinhold@gmail.com>
2021-04-13 22:14:26 +1000

    Add toJson to freezed return type

--- lib/src/models/creation_response.dart
+++ lib/src/models/creation_response.dart
@@ -10,4 +10,7 @@ class CreationResponse with _$CreationResponse {
 
   factory CreationResponse.fromJson(Map<String, dynamic> json) =>
       _$CreationResponseFromJson(json);
+
+  @override
+  Map<String, dynamic> toJson() => toJson();
 }
